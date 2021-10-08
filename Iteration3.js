3826f0f0f6456570020d26e5d7912e3d3baee170
Your Name <advmenendez92@gmail.com>
2021-10-08 08:17:57 +0200

    Iteration#3

---
+++ Iteration3.js
@@ -0,0 +1,7 @@
+//Usa un bucle forof para recorrer todos los destinos del array.
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (var cities of placesToTravel) {
+    console.log(cities);
+  }
+  
